584f5deb1632a8c99715d7f26c1ab7cb48c9ce56
Jeffrey Lor <jlor@hawk.iit.edu>
2020-06-25 16:39:10 -0500

    Login working

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,11 +1,15 @@
 package controllers;
 
 import dao.ILogin;
+import dao.IPokemon;
 import dao.LoginDB;
+import dao.PokemonRepoDB;
 import models.Trainer;
+import org.codehaus.jackson.map.ObjectMapper;
 import web.ConnectionManager;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 
@@ -14,14 +18,15 @@ public class LoginController
 {
 
 	@POST
-	@Path("/form")
 	public Response login_form(@FormParam("username") String username, @FormParam("password") String password) throws IOException
 	{
 		ILogin login = new LoginDB(ConnectionManager.getConnection());
 		Trainer trainer = login.login(username, password);
 		if(trainer != null)
 		{
-			return Response.status(201).build();
+			ObjectMapper mapper = new ObjectMapper();
+			String response = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(trainer);
+			return Response.status(200).entity(response).type(MediaType.APPLICATION_JSON_TYPE).build();
 		}
 		return Response.status(401).build();
 	}
