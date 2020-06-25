8c18daa0021db763514c5421701f7a3b057b6894
Jeffrey Lor <jlor@hawk.iit.edu>
2020-06-25 16:24:44 -0500

    Login Form

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -5,20 +5,17 @@ import dao.LoginDB;
 import models.Trainer;
 import web.ConnectionManager;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 
 @Path("/login")
 public class LoginController
 {
+
 	@POST
-	@Consumes("application/json")
-	public Response login_form(@FormParam("username") String username,
-							   @FormParam("password") String password) throws IOException
+	@Path("/form")
+	public Response login_form(@FormParam("username") String username, @FormParam("password") String password) throws IOException
 	{
 		ILogin login = new LoginDB(ConnectionManager.getConnection());
 		Trainer trainer = login.login(username, password);
@@ -26,7 +23,7 @@ public class LoginController
 		{
 			return Response.status(201).build();
 		}
-
 		return Response.status(401).build();
 	}
+
 }
